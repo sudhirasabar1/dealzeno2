f8c9479e2782575d5f29be74b60b229378790e57
Sudhira Sam <sudhirsabar888@gmail.com>
2026-09-07 12:00:19 +0530

    Update products.js

--- data/products.js
+++ data/products.js
@@ -3,10 +3,10 @@ const products = [
         id: 1,
         name: "Zebronics Zeb-Thunder Wireless BT Headphone",
         category: "Electronics",
-        image: "../images/zebronics-zeb-thunder.jpg",
+        image: "images/zebronics-zeb-thunder.jpg",
         oldPrice: 1199,
         price: 699,
         discount: 42,
-        affiliateLink: "PASTE_YOUR_AFFILIATE_LINK_HERE"
+        affiliateLink: "https://earnkaro.com/zebronics-zeb-thunder-wireless-bt-headphone/PPS18-Tatacliq33"
     }
 ];
