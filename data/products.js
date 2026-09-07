092b4fcec79a98dc9e6d9027b69f61cce2b6cf81
Sudhira Sam <sudhirsabar888@gmail.com>
2026-09-07 11:54:11 +0530

    Create products.js

---
+++ data/products.js
@@ -0,0 +1,12 @@
+const products = [
+    {
+        id: 1,
+        name: "Zebronics Zeb-Thunder Wireless BT Headphone",
+        category: "Electronics",
+        image: "../images/zebronics-zeb-thunder.jpg",
+        oldPrice: 1199,
+        price: 699,
+        discount: 42,
+        affiliateLink: "PASTE_YOUR_AFFILIATE_LINK_HERE"
+    }
+];
